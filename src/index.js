dde9a2e455f91752da5a922e1297e1f79ff70f7d
AhmedMoostafa <ahmed.moahmed442@gmail.com>
2021-02-05 10:54:54 +0200

    slove error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const taskRouter=require('./routers/task')
 const userRouter=require('./routers/user')
 const sessionExpress=require('express-session')
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT;
 app.use(sessionExpress({secret:'ahmed',saveUninitialized:false,resave:false}))
 app.use(express.json());
 app.use(taskRouter);
