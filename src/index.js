0852e6e4b3eea9aca596bfb36a8f8ff4e296c1c7
ahmed mostafa <57231433+AhmedMoostafa@users.noreply.github.com>
2021-07-12 12:59:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const userRouter=require('./routers/user')
 const sessionExpress=require('express-session')
 const app = express();
 const port = process.env.PORT;
-app.use(sessionExpress({secret:'ahmed',saveUninitialized:false,resave:false}))
 app.use(express.json());
 app.use(taskRouter);
 app.use(userRouter);
